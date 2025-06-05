cc6b39a635bfbe3b466e5db932f46aa62e140b95
enm-style <enmstyle@gmail.com>
2025-06-05 21:19:53 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import './App.css'
+
 function App() {
   return (
-    <div>
-      <h1>Welcome to ERP Demo App</h1>
+    <div className="App">
+      <h1>ERP App Loaded Successfully!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
