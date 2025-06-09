9ca26a40f5419882816ded69fa505ea47979afbc
enm-style <enmstyle@gmail.com>
2025-06-09 21:15:57 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react'
+
 function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: "center", marginTop: "50px" }}>Welcome to ERP Demo App</h1>
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>✅ ERP App Loaded Successfully!</h1>
     </div>
   )
 }
